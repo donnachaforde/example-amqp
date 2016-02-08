cce54dc0bcc4a0c99ff919f690ecf34b2d63ce2c
Donnacha Forde <dforde@tycoint.com>
2016-02-08 12:28:28 +0000

    Initial Commit

---
+++ rabbitmq-publisher/src/main/java/io/demo/Main.java
@@ -0,0 +1,33 @@
+package io.demo;
+/**
+ * Copyright (c) 2015 Donnacha Forde. All rights reserved.
+ *
+ */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main - <desc>
+ *
+ * @author Donnacha Forde
+ * @version Version 0.1 Sep 2015
+ * @since X0.0.1
+ */
+@SpringBootApplication
+public class Main
+{
+
+    //-------------------------------------------------------------------------
+    // main statement
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        System.out.println("**************************************************************************************");
+        System.out.println("Starting Message Publisher....");
+        SpringApplication.run(Application.class, args);
+        System.out.println("Finished Message Publisher.");
+        System.out.println("**************************************************************************************");
+    }
+}
